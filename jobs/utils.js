9c789428eb47bcc31a26baabc1442c3a0ba492c4
nlicitra <nickerous@gmail.com>
2021-05-13 10:40:16 -0400

    Stuff

--- jobs/utils.js
+++ jobs/utils.js
@@ -21,7 +21,7 @@ function formatSSL(ssl) {
     throw new Error(`SSL: ${ssl}, is not in a valid format`);
   }
   const [square, lot] = pairs;
-  let suffix = "";
+  let suffix = '';
 
   const match = square.match(/([a-z]+)?\d+([a-z]+)?/i);
   if (match) {
@@ -33,7 +33,7 @@ function formatSSL(ssl) {
       suffix = match[2];
     }
   }
-  const sq = suffix.length ? square.replace(suffix, "") : square;
+  const sq = suffix.length ? square.replace(suffix, '') : square;
   return `${(sq.padStart(4, '0') + suffix.toUpperCase()).padEnd(8, ' ')}${lot.padStart(4, '0')}`;
 }
 
@@ -50,8 +50,39 @@ function monthDiff(d1, d2) {
   return timeDiff / secondsPerMonth;
 }
 
+const formatOwnerName = (_name) => {
+  try {
+    let parts = _name.split(',').map((p) => p.trim());
+    const name = parts.length > 1 ? `${parts[1]} ${parts[0]}` : parts[0];
+    parts = name.split(' ');
+    const first = parts[0].replace(/[\W0-9]/gi, '');
+    let last = parts.length > 1 ? parts.pop() : undefined;
+    if (last) {
+      last = last.replace(/[\W0-9]/gi, '');
+    }
+    return last ? `${first} ${last}` : first;
+  } catch {
+    return _name;
+  }
+};
+
+const assembleAddress = (address1, address2, citystzip) => {
+  try {
+    let address = address1;
+    if (address2 && !address1.endsWith(address2)) {
+      address += ` ${address2}`;
+    }
+    const [city, state, zip] = citystzip.split(' ').filter((x) => x !== '');
+    return `${address}, ${city} ${state} ${zip.split('-')[0]}`;
+  } catch {
+    return 'ERROR';
+  }
+};
+
 module.exports.generateId = generateId;
 module.exports.forceCollection = forceCollection;
 module.exports.formatSSL = formatSSL;
 module.exports.createCSVString = createCSVString;
 module.exports.monthDiff = monthDiff;
+module.exports.formatOwnerName = formatOwnerName;
+module.exports.assembleAddress = assembleAddress;
